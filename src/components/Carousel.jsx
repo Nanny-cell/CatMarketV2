6bb88670cb7323489e11340908e9be08b3e1d794
Danitza Peña <danitza.p.96@gmail.com>
2024-11-26 22:32:29 -0300

    Absolute/Bullet/stash

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,19 +2,19 @@ import Image from "next/image";
 
 export const Carousel = () => {
     return (
-        <div className="relative h-[300px] bg-slate-200">
+        <div className="relative h-[300px] bg-black flex items-center justify-center">
             <Image
                 src="/gato.webp"
                 alt="Next.js Logo"
                 fill
-                priority
-                className="object-cover"
+                className="object-cover opacity-35"
             />
-            <div className="w-[300px] h-[20px] bg-black ">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+            <h2 className="text-3xl">Cat Market</h2>
+            <div className="w-32 h-[20px] flex justify-evenly items-center  absolute bottom-2 left-[50%] translate-x-[-50%]">
+                <div className="w-3 h-3 bg-slate-300 rounded-full"></div>
+                <div className="w-3 h-3 bg-slate-300 rounded-full"></div>
+                <div className="w-3 h-3 bg-slate-300 rounded-full"></div>
+                <div className="w-3 h-3 bg-slate-300 rounded-full"></div>
             </div>
         </div>
     );
